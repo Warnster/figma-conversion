7be8ba98fa59dd1705e74cf5366f828635fdf59a
lewis <lwarnster@gmail.com>
2021-08-03 17:46:36 +0100

    added all links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,45 @@
 
 export function Header(){ 
 
+    const navbarLinks = [
+        {
+            text: 'Destinations',
+            active: true
+        },
+        {
+            text: 'Hotels',
+            active: false
+        },
+        {
+            text: 'Flights',
+            active: false
+        },
+        {
+            text: 'Bookings',
+            active: false
+        },
+        {
+            text: 'Login',
+            active: false
+        },
+        {
+            text: 'Signup',
+            active: false
+        },
+        {
+            text: 'EN',
+            active: false
+        },
+    ]
+
     return <div className="header-container">
         <div className="logo"> <img src="/Logo.png" /> </div>
-        <span className="destinations"> Destinations </span>
-       
 
+        <span className="nav-link-container">
+        {navbarLinks.map((link) => {
+            return <span className={`destinations ${link.active === true ? 'active' : ''}`}> {link.text} </span>
+        })}
+        </span> 
     </div>
 
    
