122079e2fdba9eb6b70bfe210b1082d89771a040
Anussara Promadee <sara@anussaras-air.home>
2021-08-03 17:36:29 +0100

    added CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import { Header } from './components/header/Header'
+
 function App() {
   return (
     <div className="app-container">
-      <div className= "header"> 
+      <div className= "header">
       <Header/> 
         </div>
+      <div className="hero"></div>
+      
+
+
+
+ 
+
     </div>
     
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
 
 
 export function Header(){ 
-    return <div>
+
+    return <div className="header-container">
         <div className="logo"> <img src="/Logo.png" /> </div>
-        
+        <span className="destinations"> Destinations </span>
+       
+
     </div>
 
    
